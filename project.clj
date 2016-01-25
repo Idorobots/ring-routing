d378252ab8cff37c7d37969ec1cb641fd10a0215
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-25 11:53:00 +0100

    Added Codox to the project.clj.

--- project.clj
+++ project.clj
@@ -8,4 +8,7 @@
   :profiles {:dev {:dependencies [[ring/ring-mock "0.2.0"]]}}
   :plugins [[jonase/eastwood "0.1.5"]
             [lein-ancient "0.5.4"]
-            [lein-cloverage "1.0.2"]])
+            [lein-cloverage "1.0.2"]
+            [lein-codox "0.9.1"]]
+  :codox {:metadata {:doc/format :markdown}
+          :source-uri "https://github.com/Idorobots/ring-routing/blob/{version}/{filepath}#L{line}"})
